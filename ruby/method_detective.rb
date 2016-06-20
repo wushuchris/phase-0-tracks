7f724e098e2aeb7b27b2fc799da31fd751ca440f
wushuchris <chrismendoza8@gmail.com>
2016-06-20 10:19:00 -0700

    method detective

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,37 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+# "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+# "zom".insert(2,'o')
 # => “zoom”
 
-# "enhance".<???>
+# "enhance".center(12)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+# "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+# "the usual".concat("suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+#" suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+# "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+# "The mystery of the missing first letter".slice(1..-1)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+# "Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+#"zz".codepoints
+# => 122
+# (What is the significance of the number 122 in relation to the character z?) => 122 is the unicode point for that letter.
 
-# "How many times does the letter 'a' appear in this string?".<???>
+
+# "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
