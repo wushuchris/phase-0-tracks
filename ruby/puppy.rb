5d3c5ffac40674a2d611c041589e0ccae0892b9b
wushuchris <chrismendoza8@gmail.com>
2016-06-27 10:11:45 -0700

    added rollover and dog years methods

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -23,6 +23,17 @@ class Puppy
 
   def woof(number_of_woofs)
     puts "Woof!" * number_of_woofs
+    number_of_woofs
+  end
+
+  def roll_over(number_of_rolls)
+    puts "*rolls over*" * number_of_rolls
+    number_of_rolls
+  end
+
+  def dog_years(human_years)
+    dog_years = 7 * human_years
+    dog_years
   end
 
 end
@@ -32,3 +43,5 @@ end
 puppy1 = Puppy.new
 puppy1.fetch("tennis ball")
 puppy1.woof(2)
+puppy1.roll_over(3)
+p puppy1.dog_years(2)
