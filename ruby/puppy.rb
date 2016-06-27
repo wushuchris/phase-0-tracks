de26312a94833202229c262c004707db67c04fb4
wushuchris <chrismendoza8@gmail.com>
2016-06-27 10:37:32 -0700

    cleanup files

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -13,35 +13,3 @@
 #Jump
 #Run
 
-# Challenge 6.2
-class Puppy
-
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
-
-  def woof(number_of_woofs)
-    puts "Woof!" * number_of_woofs
-    number_of_woofs
-  end
-
-  def roll_over(number_of_rolls)
-    puts "*rolls over*" * number_of_rolls
-    number_of_rolls
-  end
-
-  def dog_years(human_years)
-    dog_years = 7 * human_years
-    dog_years
-  end
-
-end
-
-#driver code
-
-puppy1 = Puppy.new
-puppy1.fetch("tennis ball")
-puppy1.woof(2)
-puppy1.roll_over(3)
-p puppy1.dog_years(2)
