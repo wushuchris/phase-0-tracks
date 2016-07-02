278232a6e93563dcca565f3d56473b5cdd7c47da
wushuchris <chrismendoza8@gmail.com>
2016-07-02 08:29:01 -0700

    commit with pseudo

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,28 @@
+# Method to create a list
+
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps:
+  # [fill in any steps here]
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: {hash}
+
+# Method to add an item to a list = name the method add_item
+# input: item name and optional quantity = pass(item,qty)
+# steps:
+# output: {hash with the item(s), qty}
+
+# Method to remove an item from the list = name the method delete_item
+# input: pass the key into to method to delete item
+# steps:
+# output: {hash with the item(s), qty}
+
+# Method to update the quantity of an item = name the method update_qty
+# input: pass(item,qty) *** check to see if adding or overriding exiting hash.
+# steps:
+# output: {hash with item(s), qty}
+
+# Method to print a list and make it look pretty = name the method print_list
+# input:
+# steps:
+# output: {hash with item(s), qty} with
