a747cdf85371102d703a522f0d43d96a1bf297b1
wushuchris <chrismendoza8@gmail.com>
2016-08-04 17:37:53 -0700

    updated code

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,53 +1,53 @@
-def encrypt(password)
-    index = 0
-    until index == password.length
-      if password[index] == "z"
-        password[index] = "a"
-      elsif password[index] == " "
-        password[index] = " "
-      else
-      password[index] = password[index].next
-      end
-      index += 1
-    end
- end
+#4-6 Christopher Mendoza & Krista Prokopczyk
 
-def decrypt(password)
-   index = 0
-   alpha = "abcdefghijklmnopqrstuvwxyz"
-   until index == password.length
-    if password[index] == " "
-      password[index] = " "
-    else
-     password[index] = alpha[alpha.index(password[index])-1]
-    end
-    index += 1
-   end
+def encrypt(string)
+  string = string
+  encrypt_string = ""
+  index_counter = 0
+  until index_counter == string.length
+    encrypt_string[index_counter] = string[index_counter].next
+    index_counter += 1
+  end
+  return encrypt_string
 end
 
+def decrypt(string)
+  alpha = "abcdefghijklmnopqrstuvwxyz"
+  string = string
+  decrypt_string = ""
+  index_counter = 0
+  until index_counter == string.length
+    decrypt_string[index_counter] = alpha[alpha.index(string[index_counter]) - 1 ]
+    index_counter += 1
+  end
+  return decrypt_string
+end
 
+#DRIVER CODE
 
+#encrypt("abc") should return "bcd"
+#p encrypt("abc")
+#encrypt("zed") should return "afe"
+#p encrypt("zed")
+#decrypt("bcd") should return "abc"
+#p decrypt("bcd")
+#decrypt("afe") should return "zed"
+#p decrypt("afe")
+#nested call should work because it works from the "inside out" of the parenthases.
+#p decrypt(encrypt("swordfish"))
+
+# USER INTERFACE
 puts "Would you like to encrypt(1) or decrypt(2)"
-selector = gets.to_i
-
-puts "Enter Password"
+selection = gets.to_i
+puts "Please Enter Password"
 password = gets.chomp
 
-if selector == 1
-  encrypt(password)
-elsif selector == 2
-  decrypt(password)
-else
-  puts "Invalid input"
+if selection == 1
+  encrypted_pass = encrypt(password)
+  puts "The encrypted password is #{encrypted_pass}"
+  elsif selection == 2
+    decrypted_pass = decrypt(password)
+    puts "The decrypted password is #{decrypted_pass}"
+  else
+    puts "I don't understand"
 end
-
-p password
-
-# I Cannot figure this out! I don't understand why it doesn't work
-#decrypt(encrypt(password))
-#p password
-
-#encrypt(password)
-#p password
-#decrypt(password)
-#p password
